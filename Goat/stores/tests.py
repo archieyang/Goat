101ca5a8efd56b0fe2f329f3b77f51eb46708ed7
Archie <yangyxcn@gmail.com>
2015-02-20 14:37:42 +0800

    Implement creating stores
    Signed-off-by: Archie <yangyxcn@gmail.com>

--- Goat/stores/tests.py
+++ Goat/stores/tests.py
@@ -2,6 +2,7 @@
 import json
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from rest_framework import status
 from rest_framework.renderers import JSONRenderer
 from Goat.stores.models import Store
 from Goat.stores.serializers import StoreSerializer
@@ -36,6 +37,26 @@ class StoreListTests(TestCase):
 
     def test_get_store_list(self):
         response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(json.dumps(StoreSerializer(Store.objects.all(), many=True).data),
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(JSONRenderer().render(StoreSerializer(Store.objects.all(), many=True).data),
                          response.content)
+
+
+class StoreCreateTest(TestCase):
+    def setUp(self):
+        self.url = reverse('stores:list')
+
+    def test_create_store_success(self):
+        storeData = {
+            'name': 'Store',
+            'location': 'Location',
+            'category': 'Category'
+        }
+        response = self.client.post(
+            self.url,
+            json.dumps(storeData),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.content, json.dumps(StoreSerializer(Store.objects.last()).data))
--- Goat/stores/views.py
+++ Goat/stores/views.py
@@ -1,17 +1,22 @@
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from Goat.stores.models import Store
 from Goat.stores.serializers import StoreSerializer
 
 
-def get_index(request):
-    return HttpResponse("hello world")
-
-
+@api_view(['GET', 'POST'])
 def store_list(request):
     if request.method == 'GET':
         stores = Store.objects.all()
         serializer = StoreSerializer(stores, many=True)
-        return JsonResponse(serializer.data, safe=False)
-    else:
-        return None
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = StoreSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
