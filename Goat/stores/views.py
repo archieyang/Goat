82e147413a6d6d57d917a7429000aadad689c95a
Archie <yangyxcn@gmail.com>
2015-02-20 18:59:37 +0800

    Refactor store views to user general class-based views
    Signed-off-by: Archie <yangyxcn@gmail.com>

--- Goat/stores/views.py
+++ Goat/stores/views.py
@@ -1,19 +1,10 @@
 from rest_framework import generics
-from rest_framework import mixins
 
 from Goat.stores.models import Store
 from Goat.stores.serializers import StoreSerializer
 
 
-class StoreList(mixins.ListModelMixin,
-                mixins.CreateModelMixin,
-                generics.GenericAPIView):
+class StoreList(generics.ListCreateAPIView):
 
     queryset = Store.objects.all()
     serializer_class = StoreSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
