a589819b490a152ef51474159fa1e4c2b5ac0e9b
Archie <yangyxcn@gmail.com>
2015-02-21 14:13:26 +0800

    add hyperlink in user list
    Signed-off-by: Archie <yangyxcn@gmail.com>

--- Goat/users/serializers.py
+++ Goat/users/serializers.py
@@ -2,7 +2,11 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    stores = serializers.HyperlinkedIdentityField(many=True,
+                                                  view_name='stores:detail',
+                                                  read_only=True)
+
     class Meta:
         model = User
         fields = ['id', 'username', 'stores']
